5233ed5dd6cbfe0c077a42c862dfd8f0e8733c1d
Hector Biminchumo <hectorbiminchumo@gmail.com>
2025-01-06 15:16:00 -0500

    feat: updating repository

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,6 @@ import { CarCard, CustomFilter, Hero, SearchBar, ShowMore } from "@/components";
 import { fuels, yearsOfProduction } from "@/constants";
 import { CarProps, HomeProps } from "@/types";
 import { fetchCars } from "@/utils";
-import Image from "next/image";
 
 export default async function Home({ searchParams }: HomeProps ) {
   
